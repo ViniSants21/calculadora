f40d81e74f483093361765aff9958317a549ab10
ViniSants21 <vinicius.santos23@gmail.com>
2026-04-27 14:03:04 -0300

    second

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -7,115 +7,250 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calculadora SENAI',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _display = '0';
+  double _resultado = 0;
+  String _operacao = '';
+  bool _novoNumero = true;
 
-  void _incrementCounter() {
+  void _pressionarNumero(String numero) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_novoNumero) {
+        _display = numero;
+        _novoNumero = false;
+      } else {
+        if (_display == '0') {
+          _display = numero;
+        } else {
+          _display += numero;
+        }
+      }
     });
+  }
+
+  void _pressionarOperacao(String operacao) {
+    setState(() {
+      double numeroAtual = double.parse(_display);
+      
+      if (_operacao.isNotEmpty) {
+        _calcular();
+      } else {
+        _resultado = numeroAtual;
+      }
+      
+      _operacao = operacao;
+      _novoNumero = true;
+    });
+  }
+
+  void _calcular() {
+    setState(() {
+      double numeroAtual = double.parse(_display);
+      
+      switch (_operacao) {
+        case '+':
+          _resultado += numeroAtual;
+          break;
+        case '-':
+          _resultado -= numeroAtual;
+          break;
+        case '*':
+          _resultado *= numeroAtual;
+          break;
+        case '/':
+          if (numeroAtual != 0) {
+            _resultado /= numeroAtual;
+          }
+          break;
+      }
+      
+      _display = _resultado.toString();
+      _operacao = '';
+      _novoNumero = true;
+    });
+  }
+
+  void _limpar() {
+    setState(() {
+      _display = '0';
+      _resultado = 0;
+      _operacao = '';
+      _novoNumero = true;
+    });
+  }
+
+  void _adicionarVirgula() {
+    setState(() {
+      if (!_display.contains('.')) {
+        _display += '.';
+        _novoNumero = false;
+      }
+    });
+  }
+
+  Widget _botao(
+    String texto, {
+    VoidCallback? onPressed,
+    Color? corFundo,
+  }) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: corFundo ?? Colors.grey[300],
+            foregroundColor: Colors.black,
+            padding: const EdgeInsets.symmetric(vertical: 24),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: Text(
+            texto,
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Calculadora SENAI'),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: [
+          // Display
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            color: Colors.grey[900],
+            child: Text(
+              _display,
+              textAlign: TextAlign.right,
+              style: const TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          // Botões
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    // Row 1: C, /, *, -
+                    Row(
+                      children: [
+                        _botao('C',
+                            onPressed: _limpar,
+                            corFundo: Colors.red[300]),
+                        _botao('/',
+                            onPressed: () => _pressionarOperacao('/'),
+                            corFundo: Colors.orange[300]),
+                        _botao('*',
+                            onPressed: () => _pressionarOperacao('*'),
+                            corFundo: Colors.orange[300]),
+                        _botao('-',
+                            onPressed: () => _pressionarOperacao('-'),
+                            corFundo: Colors.orange[300]),
+                      ],
+                    ),
+                    // Row 2: 7, 8, 9, +
+                    Row(
+                      children: [
+                        _botao('7', onPressed: () => _pressionarNumero('7')),
+                        _botao('8', onPressed: () => _pressionarNumero('8')),
+                        _botao('9', onPressed: () => _pressionarNumero('9')),
+                        _botao('+',
+                            onPressed: () => _pressionarOperacao('+'),
+                            corFundo: Colors.orange[300]),
+                      ],
+                    ),
+                    // Row 3: 4, 5, 6
+                    Row(
+                      children: [
+                        _botao('4', onPressed: () => _pressionarNumero('4')),
+                        _botao('5', onPressed: () => _pressionarNumero('5')),
+                        _botao('6', onPressed: () => _pressionarNumero('6')),
+                      ],
+                    ),
+                    // Row 4: 1, 2, 3
+                    Row(
+                      children: [
+                        _botao('1', onPressed: () => _pressionarNumero('1')),
+                        _botao('2', onPressed: () => _pressionarNumero('2')),
+                        _botao('3', onPressed: () => _pressionarNumero('3')),
+                      ],
+                    ),
+                    // Row 5: 0, ., =
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              onPressed: () => _pressionarNumero('0'),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey[300],
+                                foregroundColor: Colors.black,
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 24),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                              ),
+                              child: const Text(
+                                '0',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        _botao('.',
+                            onPressed: _adicionarVirgula,
+                            corFundo: Colors.grey[300]),
+                        _botao('=',
+                            onPressed: _calcular,
+                            corFundo: Colors.green[300]),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
